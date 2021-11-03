b34239de49040168ba7dcb54b971e473754e5a4d
Sam White <sam.white@adder.com>
2021-11-03 17:12:47 +0000

    Now it only checks the teams that are going to be playing. Does not include postponed matches

--- max_min_positions.py
+++ max_min_positions.py
@@ -22,10 +22,17 @@ def is_team(class_):
 
 upcoming_match_block = soup.find(class_=is_match_block)
 
+print(upcoming_match_block.find("h3").string)
+
 fixtures = []
 for f in upcoming_match_block.find_all(class_=is_fixture):
     match = [t.string for t in f.find_all(class_=is_team)]
     fixtures.append(match)
+
+teams_to_check = [team for match in fixtures for team in match]
+
+
+
 
 # get the current table (thanks Gav)
 current_table = pd.read_html("https://www.twtd.co.uk/league-tables/", index_col=0, header=0)[1]
@@ -35,21 +42,21 @@ current_table = current_table.drop(["GF", "GA", "W", "D", "L", "GF.1", "GA.1", "
 current_table.loc[1, "Max pos"]    = 1
 current_table.loc[1, "GD Max pos"] = 1
 # look at each row in turn, find max position
-for i in range(2,25):
-    pts           = current_table.loc[i, "Pts"]
-    pts_after_win = current_table.loc[i, "Pts"] + 3
-    gd            = current_table.loc[i, "GD"] + 3
+for t in teams_to_check:
+    pts           = current_table.loc[current_table.Team == t].Pts.values[0]
+    pts_after_win = current_table.loc[current_table.Team == t].Pts.values[0] + 3
+    gd            = current_table.loc[current_table.Team == t].GD.values[0]  + 3
 
     # absolute maximum position, no restrictions. Just 3 points added
     absolute_max_pos   = 25-current_table.sort_values(["Pts", "GD"], ascending=True)["Pts"].searchsorted([pts_after_win] , side="right")
-    current_table.loc[i, "Max pos"] = absolute_max_pos
+    current_table.loc[current_table.Team == t, "Max pos"] = absolute_max_pos
 
     # find any positions that are only attainable with a GD swing >=4
     gd_table = current_table[current_table.Pts == pts_after_win]
     gd_table_sorted = gd_table.sort_values("GD", ascending=True)
     likely_max_pos = absolute_max_pos + (len(gd_table_sorted) - gd_table_sorted.GD.searchsorted(gd, side="right"))
 
-    current_table.loc[i, "GD Max pos"] = likely_max_pos
+    current_table.loc[current_table.Team == t, "GD Max pos"] = likely_max_pos
 
     fixtures_copy = [f[:] for f in fixtures]
 
@@ -57,30 +64,23 @@ for i in range(2,25):
     mini_table = current_table[(current_table.Pts > pts) & (current_table.Pts <= pts_after_win)].Team
     teams_on_3 = current_table[(current_table.Pts == pts_after_win)].Team
     max_poss_pos = absolute_max_pos
-    for t in (teams_on_3): 
+    for t3 in (teams_on_3): 
         for match in fixtures_copy:
-            if t in match:
-                match.remove(t)
+            if t3 in match:
+                match.remove(t3)
                 if match != []:
                     if match[0] in mini_table.values:
                         max_poss_pos += 1
 
-    current_table.loc[i, "Max poss pos"] = max_poss_pos
+    current_table.loc[current_table.Team == t, "Max poss pos"] = max_poss_pos
 
-
-    #print(current_table[current_table.Pts == pts_after_win].Team)
-
-
-# we know team #24 can't go lower
-current_table.loc[24, "Min pos"]    = 24
-current_table.loc[24, "GD min pos"] = 24
 
 # minimum position - rudimentary, might not be 100% accurate
 # we look at the number of teams below in the league that have a max_pos >= this team's current position
+# a team can potentially move down even if they're not playing, so we check all teams
 for i in range(1,25):
     current_table.loc[i, "Min pos"] = i + len(current_table.loc[i+1:][current_table.loc[i+1:]["Max pos"] <= i])
     current_table.loc[i, "GD min pos"] = i + len(current_table.loc[i+1:][current_table.loc[i+1:]["GD Max pos"] <= i])
 
 print(current_table)
-print(fixtures)
 
