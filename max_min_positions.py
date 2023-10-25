7ba5b6284ce54a13fe38e1208988a407952c2385
Sam White <sam.white@adder.com>
2023-10-25 17:52:59 +0100

    OO approach started

--- max_min_positions.py
+++ max_min_positions.py
@@ -10,11 +10,36 @@ import os
 import argparse
 import os
 
+class Team:
+    def __init__(self, pos, entry):
+        self.pos = pos
+        self.name = entry.Team
+        self.points = entry.Pts
+        self.gd = entry.GD
+        self.is_playing = False
+
+def find_max_position(team, teams):
+    team.max_pos = team.pos
+    if team.is_playing:
+        team.teams_reachable = [t for t in teams[:team.pos-1] if t.points <= team.points + 3]
+        team.max_pos -= len(team.teams_reachable)
+    else:
+        team.teams_reachable = [t for t in teams[:team.pos-1] if t.points == team.points]
+        team.max_pos -= len([t for t in team.teams_reachable if t.is_playing])
+
+def find_min_position(team, teams):
+    team.min_pos = team.pos
+    team.teams_chasing = [t for t in teams[team.pos:] if t.points >= team.points - 3]
+    team.min_pos += len([t for t in team.teams_chasing if t.max_pos <= team.pos])
+
+
+
+
 # argument handling
 parser = argparse.ArgumentParser(description="Display what a league table could \
         look like after the next round of fixtures")
 
-parser.add_argument('competition', metavar='competition', default='league-one',
+parser.add_argument('competition', metavar='competition', default='championship',
         choices=["premier-league", "championship", "league-one", "league-two"],
         nargs='?', help="The competition to analyse")
 
@@ -46,7 +71,7 @@ def collect_fixtures(match_block, fixtures, postponed):
         match_ = [t.string for t in f.find_all(class_=is_team)]
         if f.find(class_="sp-c-fixture__block sp-c-fixture__block--time gel-brevier"):
             fixtures.append(match_)
-        else: 
+        else:
             for t in match_:
                 postponed[t] = f.find(class_="gel-brevier sp-c-fixture__status").text
 
@@ -82,15 +107,6 @@ for f in fixtures:
 
 teams_to_check = [team for match_ in fixtures for team in match_]
 
-#try:
-#    teams_to_check[teams_to_check.index("Brighton & Hove Albion")] = "Brighton and Hove Albion"
-#except:
-#    pass
-#try:
-#    teams_to_check[teams_to_check.index("Milton Keynes Dons")] = "MK Dons"
-#except:
-#    pass
-
 # get the current table (thanks Gav)
 if args.debug:
     current_table = pd.read_html("debug_table.html", index_col=0, header=0)[1]
@@ -100,151 +116,18 @@ current_table = current_table.drop(["GF", "GA", "W", "D", "L", "GF.1", "GA.1", "
 
 num_teams = len(current_table.Team)+1
 
-# we know team #1 can't go higher
+teams = []
+
 for i in range(1,num_teams):
-    current_table.loc[i, "Max pos"]      = i
-    current_table.loc[i, "GD Max pos"]   = i
-    current_table.loc[i, "Max poss pos"] = i
+    teams.append(Team(i, current_table.loc[i]))
 
-# look at each row in turn, find max position
-for t in teams_to_check:
-    pts           = current_table.loc[current_table.Team == t].Pts.values[0]
-    pts_after_win = current_table.loc[current_table.Team == t].Pts.values[0] + 3
-    gd            = current_table.loc[current_table.Team == t].GD.values[0]  + 3
+for t in teams:
+    t.is_playing = True if t.name in teams_to_check else False
+    find_max_position(t, teams)
 
-    # absolute maximum position, no restrictions. Just 3 points added
-    absolute_max_pos   = num_teams-current_table.sort_values(["Pts", "GD"], ascending=True)["Pts"].searchsorted([pts_after_win] , side="right")
-    current_table.loc[current_table.Team == t, "Max pos"] = absolute_max_pos
+for t in teams:
+    find_min_position(t, teams)
 
-    # find any positions that are only attainable with a GD swing >=4
-    gd_table = current_table[current_table.Pts == pts_after_win]
-    gd_table_sorted = gd_table.sort_values("GD", ascending=True)
-    likely_max_pos = absolute_max_pos + (len(gd_table_sorted) - gd_table_sorted.GD.searchsorted(gd, side="right"))
-
-    current_table.loc[current_table.Team == t, "GD Max pos"] = likely_max_pos
-
-    # find any impossible positions
-    fixtures_copy = [f[:] for f in fixtures]
-    mini_table = current_table[(current_table.Pts > pts) & (current_table.Pts <= pts_after_win)].Team
-    teams_on_3 = current_table[(current_table.Pts == pts_after_win)].Team
-    max_poss_pos = absolute_max_pos
-    for t3 in (teams_on_3):
-        for match_ in fixtures_copy:
-            if t3 in match_:
-                match_.remove(t3)
-                if match_ != []:
-                    if match_[0] in mini_table.values:
-                        max_poss_pos += 1
-
-    current_table.loc[current_table.Team == t, "Max poss pos"] = max_poss_pos
-
-
-# minimum position 
-
-# first, we look at the number of teams below in the league that have a max_pos >= this team's current position
-# a team can potentially move down even if they're not playing, so we check all teams
-for i in range(1,num_teams):
-    current_table.loc[i, "Min pos"] = i + len(current_table.loc[i+1:][current_table.loc[i+1:]["Max pos"] <= i])
-    current_table.loc[i, "GD min pos"] = i + len(current_table.loc[i+1:][current_table.loc[i+1:]["GD Max pos"] <= i])
-    pts         = current_table.loc[current_table.Team == t].Pts.values[0]
-    mini_table  = current_table[(current_table.Pts <= pts) & (current_table.Pts >= pts-1)]
-
-
-# now we want to create a new dataframe. There will be a column for each team,
-# and we iterate through the elements and fill them in with a suitable
-# character
-
-graph = []
-
-# priority
-# current position
-# impossible position
-
-for t in range(1,num_teams):
-    graph.append([])
-    abs_max_pos    = current_table.loc[t, "Max pos"]
-    max_poss_pos   = current_table.loc[t, "Max poss pos"]
-    likely_max_pos = current_table.loc[t, "GD Max pos"]
-    min_pos        = current_table.loc[t, "Min pos"]
-    likely_min_pos = current_table.loc[t, "GD min pos"]
-
-    team_name      = current_table.loc[t, "Team"]
-
-    for pos in range(1,num_teams):
-        if pos == t:
-            if os.path.exists("./img/logos/{}.png".format(re.sub(" ", "-", team_name).lower())):
-                char = '<img src="{}"></img>'.format(os.path.abspath("./img/logos/{}.png".format(re.sub(" ", "-", team_name).lower())))
-            else:
-                char = '<img src="{}"></img>'.format(os.path.abspath("./img/current.png"))
-        elif pos < t:
-            if pos < abs_max_pos:
-                char = ""
-            else:
-                if pos < max_poss_pos:
-                    char = '<img src="{}"></img>'.format(os.path.abspath("./img/impossible.png"))
-                else:
-                    if pos == max_poss_pos:
-                        if max_poss_pos >= likely_max_pos:
-                            char = '<img src="{}"></img>'.format(os.path.abspath("./img/likely_top.png"))
-                        else:
-                            char = '<img src="{}"></img>'.format(os.path.abspath("./img/unlikely_top.png"))
-                    else:
-                        if pos < likely_max_pos:
-                            char =  '<img src="{}"></img>'.format(os.path.abspath("./img/unlikely_move.png"))
-                        else:
-                            char =   '<img src="{}"></img>'.format(os.path.abspath("./img/likely_move.png"))
-        else:
-            if pos > min_pos:
-                char = ""
-            else:
-                if pos == min_pos:
-                    if min_pos != likely_min_pos:
-                        char = '<img src="{}"></img>' .format(os.path.abspath("./img/unlikely_bottom.png"))
-                    else:
-                        char = '<img src="{}"></img>' .format(os.path.abspath("./img/likely_bottom.png"))
-                else:
-                    if pos > likely_min_pos:
-                        char = '<img src="{}"></img>'.format(os.path.abspath("./img/unlikely_move_down.png"))
-                    else:
-                        char = '<img src="{}"></img>'.format(os.path.abspath("./img/likely_move_down.png"))
-
-        graph[-1].append(char)
-
-df = pd.DataFrame({})
-
-df[0] = current_table.Team
-for i in range(1,num_teams):
-    df[i] = (graph[i-1])
-
-# construct an array of opponents
-opponents = []
-for t in current_table.Team:
-    opp = ""
-    if t in postponed.keys():
-        opp = postponed[t]
-        opponents.append(f"<i><font color = Gray>{opp}</font></i>")
-    else:
-        for f in fixtures:
-            if t in f:
-                if f[0] == t:
-                    opp = f"{f[1]} (h)"
-                else:
-                    opp = f"{f[0]} (a)"
-                break
-        if opp != "":
-            opponents.append(f"<i><font color = Gray>vs. {opp}</font></i>")
-        else:
-            opponents.append("")
-
-df["Next Opponents"] = opponents
-
-styled_table = df.style.set_table_styles([
-    {'selector':''  , 'props':'border-collapse: collapse; font-family:Louis George Cafe; font-size:12px;'},
-    {'selector':'tbody tr:nth-child(2n+1)', 'props':'background: #f0f0f0;'},
-    {'selector':'tr', 'props':'line-height: 16px'},
-    {'selector':'td', 'props':'padding: 0px 5px 0px 0px;'},
-    {'selector':'th', 'props':'padding: 0px 5px;'},
-    ])
-
-imgkit.from_string(styled_table.to_html(), "out.png", options={'enable-local-file-access':'', 'quality':'100', 'crop-w':'830', 'crop-y':'23'})
+for t in teams:
+    print(t.name, t.is_playing, t.max_pos, t.min_pos)
 
